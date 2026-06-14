858e5d97b89211344ebaa5e7e3a228014f42b61d
Enk-sho portfolio <emshowah3@gmail.com>
2026-06-14 17:59:22 -0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+print("1 - Addition")
+print("2 - subtraction")
+print("3 - Multiplication")
+print("4 - Divide")
+option = int(input("Choose an operation: "))
+
+if(option in [1,2,3,4]):
+    num1 = int(input("enter first number:"))
+    num2 = int(input("enter second number:"))
+
+    if(option == 1):
+        result = num1 + num2
+    elif(option == 2):
+        result = num1 - num2
+    elif(option == 3):
+        result = num1 * num2
+    elif(option == 4):
+        result = num1 / num2
+else:
+    print("Invalid operation entered")
+print(round(result,100))
